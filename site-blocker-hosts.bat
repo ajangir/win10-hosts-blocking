418ac709ea803cb54bddd1176b78346ff2ff2daa
Ajay Jangir <ajayjangir5252@gmail.com>
2026-03-06 07:02:18 +0530

    repo backup: 2026-03-06 07:02

--- site-blocker-hosts.bat
+++ site-blocker-hosts.bat
@@ -1,3 +1,3 @@
 @echo off
-pwsh.exe -ExecutionPolicy Bypass -nop -File "C:\Users\ajay-winX\a_src\github\win10-scripts-run\win10-hosts-blocking\site-blocker-hosts.ps1" %*
-pause
+pwsh.exe -ExecutionPolicy Bypass -NoExit -File "C:\Users\ajay-winX\a_src\github\win10-scripts-run\win10-hosts-blocking\site-blocker-hosts.ps1" %*
+
